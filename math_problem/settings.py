fecb2ac7f1957dececa1bedcf9a407b07dee7ca3
wonhyuk <whcho888@gmail.com>
2018-03-01 00:40:00 +0900

    merge

--- math_problem/settings.py
+++ math_problem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+92czh3^)t4qxjpfj=vk-6m)m^c##8&+c(t9oghszlmkk@bt=m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
